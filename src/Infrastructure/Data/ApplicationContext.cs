2d10589977a304dd9262a9fe5e2563f587a64a9c
Anton <antongunko@outlook.com>
2020-12-14 17:59:41 +0300

    add Infrastructure layer and dbContext for entities, identity

---
+++ src/Infrastructure/Data/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartBooks> CartBooks { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderBooks> OrderBooks { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Like> Likes { get; set; }
+        public DbSet<FeedBack> FeedBacks { get; set; }
+    }
+}

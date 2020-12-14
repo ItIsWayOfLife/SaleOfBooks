a3c62653a12e3c8fd690ae1070264ad048820c1a
Anton <antongunko@outlook.com>
2020-12-14 18:08:27 +0300

    add initializer for book genre, user and role

---
+++ src/Infrastructure/Data/EntitiesInitializer.cs
@@ -0,0 +1,25 @@
+﻿using Core.Entities;
+using System.Linq;
+
+namespace Infrastructure.Data
+{
+    public class EntitiesInitializer
+    {
+        public static void Initialize(ApplicationContext context)
+        {
+            if (!context.Genres.Any())
+            {
+                context.Genres.AddRange(
+                    new Genre() { Name = "Drama" },
+                    new Genre() { Name = "Fantasy" },
+                    new Genre() { Name = "Fairy tale" },
+                    new Genre() { Name = "Travel books" },
+                    new Genre() { Name = "Autobiography" },
+                    new Genre() { Name = "Mystery" },
+                    new Genre() { Name = "Comics" }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}

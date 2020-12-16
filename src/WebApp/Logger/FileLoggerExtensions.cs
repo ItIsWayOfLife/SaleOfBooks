9106c388d78ce68b86bcb6f146bb01ace4ce43fe
Anton <antongunko@outlook.com>
2020-12-16 14:33:59 +0300

    add logger

---
+++ src/WebApp/Logger/FileLoggerExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Logging;
+
+
+namespace WebApp.Logger
+{
+    public static class FileLoggerExtensions
+    {
+        public static ILoggerFactory AddFile(this ILoggerFactory factory,
+                                        string filePath)
+        {
+            factory.AddProvider(new FileLoggerProvider(filePath));
+            return factory;
+        }
+    }
+}
